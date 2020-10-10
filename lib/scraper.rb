d6d8708c115755fdfb3dbe7941d681c73ad2b4ce
Carl J <carlj1881@yahoo.com>
2020-10-10 01:39:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,6 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
-puts doc.css(".headline-26OIBN").text
+
+
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
